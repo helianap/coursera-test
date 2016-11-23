0205710e8b4422d6dfb5418cc6121d8c91a67b43
Heliana Parra <Heliana Parra>
2016-11-23 16:36:43 -0500

    angular mod 1

---
+++ AngularJS/mod1_solution/Scripts/app.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+   
+    angular.module('LunchCheck', []).
+    controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope)
+    {
+        $scope.message = '';
+        $scope.lunchmenu = '';
+        $scope.selectedcss ='';
+        $scope.CheckIfTooMuch = function () {
+
+            if ($scope.lunchmenu != "") {
+                var foodItems = $scope.lunchmenu.split(",");
+                var total = 0;
+                for (var i = 0; i < foodItems.length; i++)
+                {
+                    //validation for empty strings
+                    if(foodItems[i].trim()!="")
+                        total++;
+                }
+                if (total <= 3) {
+                    $scope.message = 'Enjoy!';
+                    $scope.selectedcss = 'green';
+                }
+                    
+                else{
+                    $scope.message = 'Too much!';
+                    $scope.selectedcss = 'green';
+                }
+            }
+            else {
+                $scope.message = 'Please enter data first';
+                $scope.selectedcss = 'red';
+            }
+                
+        };
+    }
+})();
